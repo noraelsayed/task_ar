2026268a2a86a166e9b0290f202be21a64583c36
Nora Elsayed <93290859+noraelsayed@users.noreply.github.com>
2024-04-06 06:38:08 +0200

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "assets.vercel.com",
+        port: "",
+        pathname: "/image/upload/**",
+      },
+    ],
+  },
+};
